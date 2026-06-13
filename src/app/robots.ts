471f7631c551d6cad1056f80b0e65b901d12324e
Atif Jan <atifjan2019@gmail.com>
2026-06-13 23:35:18 +0500

    fix: update robots.txt to remove disallowed path for Next.js assets

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/admin/", "/api/", "/_next/"],
+        disallow: ["/admin/", "/api/"],
       },
     ],
     sitemap: "https://www.quadbiketourism.com/sitemap.xml",
